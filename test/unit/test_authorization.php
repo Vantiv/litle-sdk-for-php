5fdbe5ec2ad2f655e65a3598143d82e01a36cf9f
Jack, Gao <rgao@litle.com>
2012-02-21 17:20:55 -0500

    completed test_authorization

--- test/unit/test_authorization.php
+++ test/unit/test_authorization.php
@@ -32,15 +32,158 @@ class LitleUnitTest extends UnitTestCase
 		$litleTest->newXML = $mappTest;
 		$retOb = $litleTest->authorizationRequest($hash_in);
 	}
-	function test_auth_with_litleTxnId()
+
+	
+	function test_noOrderId()
 	{
-		$hash_in = array('litleTxnId'=>'2234567890');
-		$mappTest = &new MockLitleXmlMapper();
-		$commTest = &new Mockcommunication();
-		$mappTest->expectOnce('request',array(new PatternExpectation('/.*<authorization reportGroup="planets" id="10"><litleTxnId>2234567890.*/')));
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'litleTxnId'=>'123456',
+	      'amount'=>'106',
+	      'orderSource'=>'ecommerce',
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210')
+	      );
 		$litleTest = &new LitleOnlineRequest();
-		$litleTest->newXML = $mappTest;
+		$this->expectException(new Exception("Missing Required Field: /orderId/"));
 		$retOb = $litleTest->authorizationRequest($hash_in);
+		
+		
 	}
+	
+	function test_noAmount()
+	{
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'litleTxnId'=>'123456',
+	      'orderId'=>'12344',
+	      'orderSource'=>'ecommerce',
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210')
+		);
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Missing Required Field: /amount/"));
+		$retOb = $litleTest->authorizationRequest($hash_in);
+	
+	}
+	
+	function test_noOrderSource()
+	{
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'litleTxnId'=>'123456',
+	      'orderId'=>'12344',
+	      'amount'=>'106',
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210')
+		);
+		$litleTest = &new LitleOnlineRequest();
+	$this->expectException(new Exception("Missing Required Field: /orderSource/"));
+		$retOb = $litleTest->authorizationRequest($hash_in);
+	
+	}
+	
+	function test_BothChoicesCardandPaypal()
+	{
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'orderId'=>'12344',
+	      'amount'=>'106',
+	      'orderSource'=>'ecommerce',
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210'
+	      ),
+	      'paypal'=>array(
+	      'payerId'=>'1234',
+	      'token'=>'1234',
+	      'transactionId'=>'123456')
+		);
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Entered an Invalid Amount of Choices for a Field, please only fill out one Choice!!!!"));
+		$retOb = $litleTest->authorizationRequest($hash_in);
+	
+	}
+	
+	function test_threeChoicesCardandPaypageandPaypal()
+	{
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'orderId'=>'12344',
+	      'amount'=>'106',
+	      'orderSource'=>'ecommerce',
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210'
+	      ),
+	      'paypage'=> array(
+	      'paypageRegistrationId'=>'1234',
+	      'expDate'=>'1210',
+	      'cardValidationNum'=>'555',
+	      'type'=>'VI'),
+	      'paypal'=>array(
+	      'payerId'=>'1234',
+	      'token'=>'1234',
+	      'transactionId'=>'123456')
+		);
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Entered an Invalid Amount of Choices for a Field, please only fill out one Choice!!!!"));
+		$retOb = $litleTest->authorizationRequest($hash_in);
+	
+	}
+	
+	function test_allChoicesCardandPaypageandPaypalandToken()
+	{
+		$hash_in = array('merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'litleTxnId'=>'123456',
+	      'orderId'=>'12344',
+	      'amount'=>'106',
+	      'orderSource'=>'ecommerce',
+	      'fraudCheck'=>array('authenticationTransactionId'=>'123'),
+	      'bypassVelocityCheckcardholderAuthentication'=>array('authenticationTransactionId'=>'123'),
+	      'card'=>array(
+	      'type'=>'VI',
+	      'number' =>'4100000000000001',
+	      'expDate' =>'1210'
+	      ),
+	      'paypage'=> array(
+	      'paypageRegistrationId'=>'1234',
+	      'expDate'=>'1210',
+	      'cardValidationNum'=>'555',
+	      'type'=>'VI'),
+	      'paypal'=>array(
+	      'payerId'=>'1234',
+	      'token'=>'1234',
+	      'transactionId'=>'123456'),
+	      'token'=> array(
+	      'litleToken'=>'1234',
+	      'expDate'=>'1210',
+	      'cardValidationNum'=>'555',
+	      'type'=>'VI')
+		);
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Entered an Invalid Amount of Choices for a Field, please only fill out one Choice!!!!"));
+		$retOb = $litleTest->authorizationRequest($hash_in);
+	
+	}
+	
+
+	
+
 }
 ?>
