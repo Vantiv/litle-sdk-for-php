5164db5abe246bae37edf38ac59777177db1715c
Rushi, Ganmukhi <rganmukhi@litle.com>
2012-02-22 10:08:51 -0500

    added capture tests

--- test/unit/test_capture.php
+++ test/unit/test_capture.php
@@ -8,8 +8,27 @@ require_once realpath(dirname(__FILE__)) . '/../../lib/communication.php';
 require_once realpath(dirname(__FILE__)) . '/../../lib/LitleOnlineRequest.php';
 Mock::generate('communication');
 Mock::generate('LitleXmlMapper');
+
 class capture_UnitTest extends UnitTestCase
 {
+	function test_capture()
+	{
+		$hash_in = array('litleTxnId'=> '12312312','reportGroup'=>'Planets', 'amount'=>'123');
+		$mappTest = &new MockLitleXmlMapper();
+		$commTest = &new Mockcommunication();
+		$mappTest->expectOnce('request',array(new PatternExpectation('/.*<litleTxnId>12312312.*<amount>5000.*')));
+		$litleTest = &new LitleOnlineRequest();
+		$litleTest->newXML = $mappTest;
+		$retOb = $litleTest->captureRequest($hash_in);
+	}
+
+	function test_no_txnid()
+	{
+		$hash_in =array('reportGroup'=>'Planets','amount'=>'106');
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Missing Required Field: /litleTxnId/"));
+		$retOb = $litleTest->captureRequest($hash_in);
+	}
 
 }
 ?>
