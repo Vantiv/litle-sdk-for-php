b63f0f58d7d0489f27212a800a2ceb9aff8290be
Jack, Gao <rgao@litle.com>
2012-02-27 16:23:19 -0500

    functional token tests

---
+++ test/functional/test_token.php
@@ -0,0 +1,96 @@
+<?php
+// =begin
+// Copyright (c) 2011 Litle & Co.
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// =end
+
+require_once("../../simpletest/autorun.php");
+require_once('../../simpletest/unit_tester.php');
+require_once realpath(dirname(__FILE__)) . '/../../lib/LitleOnline.php';
+
+class token_FunctionalTest extends UnitTestCase
+{
+	function test_simpleToken()
+	{
+		$hash_in = array(
+			'merchantId' => '101',
+	      'version'=>'8.8',
+	      'reportGroup'=>'Planets',
+	      'orderId'=>'12344',
+	      'accountNumber'=>'1233456789103801');
+
+		$initilaize = &new LitleOnlineRequest();
+		$registerTokenResponse = $initilaize->registerTokenRequest($hash_in);
+		$message = Xml_parser::get_attribute($registerTokenResponse,'litleOnlineResponse','message');
+		$this->assertEqual('Valid Format',$message);
+	}
+
+	function test_simpleTokenwithpaypage()
+	{
+		$hash_in = array(
+		'merchantId' => '101',
+      'version'=>'8.8',
+      'reportGroup'=>'Planets',
+      'orderId'=>'12344',
+      'paypageRegistrationId'=>'1233456789101112');
+
+		$initilaize = &new LitleOnlineRequest();
+		$registerTokenResponse = $initilaize->registerTokenRequest($hash_in);
+		$message = Xml_parser::get_attribute($registerTokenResponse,'litleOnlineResponse','message');
+		$this->assertEqual('Valid Format',$message);
+	}
+	
+	function test_simpleTokenecheck()
+	{
+		$hash_in = array(
+			'reportGroup'=>'Planets',
+	      'merchantId' => '101',
+	      'version'=>'8.8',
+	      'orderId'=>'12344',
+	      'echeckForToken'=>array('accNum'=>'12344565','routingNum'=>'123476545'));
+	
+		$initilaize = &new LitleOnlineRequest();
+		$registerTokenResponse = $initilaize->registerTokenRequest($hash_in);
+		$message = Xml_parser::get_attribute($registerTokenResponse,'litleOnlineResponse','message');
+		$this->assertEqual('Valid Format',$message);
+	}
+	
+	function test_Tokenecheckmissingrequired()
+	{
+		$hash_in = array('merchantId' => '101',
+      'version'=>'8.8',
+      'reportGroup'=>'Planets',
+      'orderId'=>'12344',
+      'echeckForToken'=>array('routingNum'=>'132344565'));
+	
+		$litleTest = &new LitleOnlineRequest();
+		$this->expectException(new Exception("Missing Required Field: /accNum/"));
+		$retOb = $litleTest->registerTokenRequest($hash_in);
+	}
+	
+	
+
+
+
+}
+?>
