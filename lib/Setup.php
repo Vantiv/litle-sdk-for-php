a2e9404c848ac7bfbab9a297a51f8867dd9691b9
Jack, Gao <rgao@litle.com>
2012-03-08 11:10:49 -0500

    remove extra quotations

--- lib/Setup.php
+++ lib/Setup.php
@@ -44,7 +44,7 @@ function initialize(){
 			fwrite($handle, $keys .' = '. $values);
 			fwrite($handle, PHP_EOL);
 		}
-		fwrite($handle, "version = '8.10'" .  PHP_EOL);
+		fwrite($handle, "version = 8.10" .  PHP_EOL);
 		fwrite($handle, "timeout =  65".  PHP_EOL);
 		fwrite($handle, "id =  10".  PHP_EOL);
 		fwrite($handle, "reportGroup = planets".  PHP_EOL);
