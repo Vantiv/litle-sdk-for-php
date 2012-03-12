291587a82ca03c44f59961f3a0f1c9adb7c29977
Rushi <rganmukh@litle.com>
2012-03-12 10:46:52 -0400

    changed code so that if config file is not present throw appropriate error

--- lib/Obj2xml.php
+++ lib/Obj2xml.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Copyright (c) 2011 Litle & Co.
+ * Copyright (c) 2011 Litle & Co.
 *
 * Permission is hereby granted, free of charge, to any person
 * obtaining a copy of this software and associated documentation
@@ -35,55 +35,63 @@ class Obj2xml {
 		$authentication->addChild('user',$config["user"]);
 		$authentication->addChild('password',$config["password"]);
 		$transacType = $xml->addChild($type);
-		if(isset($data['partial'])) {($transacType-> addAttribute('partial',$data["partial"]));};
+		if(isset($data['partial'])) {
+			($transacType-> addAttribute('partial',$data["partial"]));
+		};
 		unset($data['partial']);
 		#merchant SDK attribute $transacType-> addAttribute('partial',$data["partial"])
-		if(isset($config['customerId'])) {($transacType-> addAttribute('customerId',$config["customerId"]));};
-		if(isset($config['reportGroup'])) {($transacType-> addAttribute('reportGroup',$config["reportGroup"]));};
-		if(isset($config['id'])) {($transacType-> addAttribute('id',$config["id"]));};
+		if(isset($config['customerId'])) {
+			($transacType-> addAttribute('customerId',$config["customerId"]));
+		};
+		if(isset($config['reportGroup'])) {
+			($transacType-> addAttribute('reportGroup',$config["reportGroup"]));
+		};
+		if(isset($config['id'])) {
+			($transacType-> addAttribute('id',$config["id"]));
+		};
 		Obj2xml::iterateChildren($data,$transacType);
 		return $xml->asXML();
 	}
-	
+
 	private function iterateChildren($data,$transacType){
 		foreach($data as $key => $value)
 		{
 			if ($value == "REQUIRED"){
 				throw new Exception("Missing Required Field: /$key/");
-			}
-			elseif (((is_string($value)) || is_numeric($value))) {
+			}elseif (((is_string($value)) || is_numeric($value))) {
 				$transacType->addChild($key,$value);
-			}
-			elseif(is_array($value))
+			}elseif(is_array($value))
 			{
 				$node = $transacType->addChild($key);
 				Obj2xml::iterateChildren($value,$node);
 			}
 		}
 	}
-	
+
 	private function getConfig($data)
-	{	
-		$config_array =parse_ini_file('litle_SDK_config.ini');
-		$names = array('user','password','merchantId','id','reportGroup','version');
-		foreach($names as $name)
-		{ 
-			if (isset($data[$name]))
-			{
-				$config[$name] = $data[$name];
-			
-			}
-			else 
-			{
-				if ($name == 'merchantId'){
-					$config['merchantId'] = $config_array['currency_merchant_map']['DEFAULT'];
+	{
+		if (file_exists(realpath(dirname(__FILE__)) . '/litle_SDK_config.ini'))
+		{
+			 	$config_array =parse_ini_file('litle_SDK_config.ini');
+				$names = array('user','password','merchantId','id','reportGroup','version');
+				foreach($names as $name)
+				{
+					if (isset($data[$name]))
+					{
+						$config[$name] = $data[$name];
+
+					}else{
+						if ($name == 'merchantId'){
+							$config['merchantId'] = $config_array['currency_merchant_map']['DEFAULT'];
+						}else {
+							$config[$name] = $config_array[$name];
+						}
+					}
 				}
-				else {
-				$config[$name] = $config_array[$name];
-				}
-			}
+				return $config;
+		}else{
+			throw new Exception("Missing Configuration File, please Run Setup.php");
 		}
-		return $config;
 	}
 }
 
