0aeaf5f4420efa640a11c9684d109dab88b181d0
Vivek, Chouhan <vchouhan@litle.com>
2013-08-07 17:38:59 -0400

    Added 2 tests for addAccountUpdate and _isfull exception respectively.

--- test/functional/test_batchRequest.php
+++ test/functional/test_batchRequest.php
@@ -42,6 +42,23 @@ class batchRequest_FunctionalTest extends PHPUnit_Framework_TestCase
 	}
 	function test_addAccountUpdate()
 	{
+		$hash_in = array(
+			'card'=>array('type'=>'VI',
+					'number'=>'4100000000000000',
+					'expDate'=>'1213',
+					'cardValidationNum' => '1213'),
+			'id'=>'1211',
+			'orderId'=> '2111',
+			'reportGroup'=>'Planets',
+			'orderSource'=>'ecommerce',
+			'amount'=>'123');
+		$batch_request = new BatchRequest($this->direct);
+		$batch_request->addAccountUpdate($hash_in);
+		
+		$this->assertTrue(file_exists($batch_request->batch_file));
+		$cts = $batch_request->getCountsAndAmounts();
+		$this->assertEquals(1, $cts['accountUpdate']['count']);
+		
 		
 	}
 	function test_addAccountUpdate_negative()
@@ -49,7 +66,26 @@ class batchRequest_FunctionalTest extends PHPUnit_Framework_TestCase
 		
 	}
 	function test_isFull(){
-		
+		try{
+			$hash_in = array(
+			'card'=>array('type'=>'VI',
+					'number'=>'4100000000000000',
+					'expDate'=>'1213',
+					'cardValidationNum' => '1213'),
+			'id'=>'1211',
+			'orderId'=> '2111',
+			'reportGroup'=>'Planets',
+			'orderSource'=>'ecommerce',
+			'amount'=>'123');
+			$batch_request = new BatchRequest($this->direct);
+			$batch_request->total_txns = MAX_TXNS_PER_BATCH;
+			$batch_request->addSale($hash_in);
+		}
+		catch(RuntimeException $expected){
+			$this->assertEquals($expected->getMessage(), "The transaction could not be added to the batch. It is full.");
+			return;
+		}
+		$this->fail('An excepted exception has not been raised');
 	}
 	function test_addTooManyTransactions()
 	{
