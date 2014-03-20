4e9fafb6751db5c9a6566a4186cc58e839c8aad4
pshah91 <pshah91@ccs.neu.edu>
2014-03-20 16:12:48 -0400

    debugging

--- lib/LitleOnlineRequest.php
+++ lib/LitleOnlineRequest.php
@@ -538,10 +538,8 @@ class LitleOnlineRequest
 		$hash_config = LitleOnlineRequest::overrideConfig($hash_in);
 
                 if($type == 'updateSubscription' || $type == 'cancelSubscription') {
-		      var_dump("Before : " . $hash_config['reportGroup']);
 //                      $hash_config  = array_diff($hash_config, array($hash_config['reportGroup']));
                       unset($hash_config['reportGroup']);
-		      var_dump("AFTER : " . $hash_config['reportGroup']);
                 }
 		
 		$hash = LitleOnlineRequest::getOptionalAttributes($hash_in,$hash_out);
