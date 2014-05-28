f52d61224b71095f8547130f51c10969c93bf0f1
wting2013 <wting2013@gmail.com>
2014-05-28 17:05:04 -0400

    Update LitleOnline.php

--- lib/LitleOnline.php
+++ lib/LitleOnline.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2011 Litle & Co. om
+ * Copyright (c) 2011 Litle & Co.
  * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
