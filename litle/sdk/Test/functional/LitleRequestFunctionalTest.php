3966286112ec36ed842e748e315504339813d3cb
arch85 <archit.agarwal@gmail.com>
2014-11-05 13:58:57 -0500

    switching xml version to 8.27

--- litle/sdk/Test/functional/LitleRequestFunctionalTest.php
+++ litle/sdk/Test/functional/LitleRequestFunctionalTest.php
@@ -60,7 +60,7 @@ class LitleRequestFunctionalTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse(file_exists($fn1));
         $this->assertFalse(file_exists($fn2));
 
-        $expected = '<batchRequest merchantId="0180" merchantSdk="PHP;9.00.0" authAmount="0" numAuths="0" saleAmount="123" numSales="1" creditAmount="0" numCredits="0" numTokenRegistrations="0"
+        $expected = '<batchRequest merchantId="0180" merchantSdk="PHP;8.27.1" authAmount="0" numAuths="0" saleAmount="123" numSales="1" creditAmount="0" numCredits="0" numTokenRegistrations="0"
         captureGivenAuthAmount="0" numCaptureGivenAuths="0" forceCaptureAmount="0" numForceCaptures="0" authReversalAmount="0" numAuthReversals="0"
         captureAmount="0" numCaptures="0" echeckVerificationAmount="0" numEcheckVerification="0" echeckCreditAmount="0" numEcheckCredit="0"
         numEcheckRedeposit="0" echeckSalesAmount="0" numEcheckSales="0" numUpdateCardValidationNumOnTokens="0"
@@ -101,7 +101,7 @@ class LitleRequestFunctionalTest extends \PHPUnit_Framework_TestCase
 
         $request->createRFRRequest(array('litleSessionId' => '8675309'));
 
-        $expected = '<litleRequest numBatchRequests="0" version="9.00" xmlns="http://www.litle.com/schema">
+        $expected = '<litleRequest numBatchRequests="0" version="8.27" xmlns="http://www.litle.com/schema">
                     <authentication><user>XXXXXX</user><password>XXXXXX</password></authentication>
                     <RFRRequest><litleSessionId>8675309</litleSessionId></RFRRequest>
                     </litleRequest>';
@@ -146,9 +146,9 @@ class LitleRequestFunctionalTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse(file_exists($fn1));
         $this->assertTrue(file_exists($request->request_file));
 
-        $expected = '<litleRequest numBatchRequests="1" version="9.00" xmlns="http://www.litle.com/schema">
+        $expected = '<litleRequest numBatchRequests="1" version="8.27" xmlns="http://www.litle.com/schema">
         <authentication><user>XXXXX</user><password>XXXXX</password></authentication>
-        <batchRequest merchantId="0180" merchantSdk="PHP;9.00.0" authAmount="0" numAuths="0" saleAmount="123" numSales="1" creditAmount="0" numCredits="0" numTokenRegistrations="0"
+        <batchRequest merchantId="0180" merchantSdk="PHP;8.27.1" authAmount="0" numAuths="0" saleAmount="123" numSales="1" creditAmount="0" numCredits="0" numTokenRegistrations="0"
         captureGivenAuthAmount="0" numCaptureGivenAuths="0" forceCaptureAmount="0" numForceCaptures="0" authReversalAmount="0" numAuthReversals="0"
         captureAmount="0" numCaptures="0" echeckVerificationAmount="0" numEcheckVerification="0" echeckCreditAmount="0" numEcheckCredit="0"
         numEcheckRedeposit="0" echeckSalesAmount="0" numEcheckSales="0" numUpdateCardValidationNumOnTokens="0" numUpdateSubscriptions="0"
