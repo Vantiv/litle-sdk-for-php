ccaa92422af8e27755943449c5597b30c4e7cd85
arch85 <archit.agarwal@gmail.com>
2014-11-05 13:59:45 -0500

    switching xml version to 8.27

--- litle/sdk/Test/unit/LitleOnlineRequestUnitTest.php
+++ litle/sdk/Test/unit/LitleOnlineRequestUnitTest.php
@@ -54,7 +54,7 @@ class LitleOnlineRequestUnitTest extends \PHPUnit_Framework_TestCase
         $mock = $this->getMock('litle\sdk\LitleXmlMapper');
         $mock->expects($this->once())
         ->method('request')
-        ->with($this->matchesRegularExpression('/.*merchantSdk="PHP;9.00.0".*/'));
+        ->with($this->matchesRegularExpression('/.*merchantSdk="PHP;8.27.1".*/'));
 
         $litleTest = new LitleOnlineRequest();
         $litleTest->newXML = $mock;
