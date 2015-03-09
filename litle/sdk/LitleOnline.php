b4013e8ee19176aa9241e6069d3c9ead717379ec
SDK Development User - Dev <sdkdev@sdkdev-vm2.dwi.litle.com>
2015-03-09 10:57:11 -0400

    updated changelog and versions

--- litle/sdk/LitleOnline.php
+++ litle/sdk/LitleOnline.php
@@ -25,7 +25,7 @@
  */
 namespace litle\sdk;
 define('CURRENT_XML_VERSION', '9.3');
-define('CURRENT_SDK_VERSION', 'PHP;9.3');
+define('CURRENT_SDK_VERSION', 'PHP;9.3.1');
 define('MAX_TXNS_PER_BATCH', 100000);
 define('MAX_TXNS_PER_REQUEST', 500000);
 define('LITLE_CONFIG_LIST', 'user,password,merchantId,timeout,proxy,reportGroup,version,url,litle_requests_path,batch_requests_path,sftp_username,sftp_password,batch_url,tcp_port,tcp_ssl,tcp_timeout,print_xml');
