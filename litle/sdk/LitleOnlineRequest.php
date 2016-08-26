5665c3a33c074573c89785ca3aa1bb56d6476f56
Scott Carpenter <scott@payforstay.com>
2016-08-26 07:21:27 -0600

    Removed debug print()

--- litle/sdk/LitleOnlineRequest.php
+++ litle/sdk/LitleOnlineRequest.php
@@ -626,7 +626,6 @@ class LitleOnlineRequest
         Checker::choice($choice1);
         Checker::choice($choice2);
         $request = Obj2xml::toXml($hash,$hash_config, $type);
-        print($request);
         $litleOnlineResponse = $this->newXML->request($request,$hash_config,$this->useSimpleXml);
 		
         return $litleOnlineResponse;
