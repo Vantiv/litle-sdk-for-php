24fd17ab27292daa24d03d8dfd7a7c7be2165ebd
qzhao <zappy0109@gmail.com>
2017-02-08 15:31:31 -0500

    Added giftCardCreditResponse

--- litle/sdk/LitleResponseProcessor.php
+++ litle/sdk/LitleResponseProcessor.php
@@ -68,7 +68,8 @@ class LitleResponseProcessor {
 				"vendorDebitResponse",
 				"physicalCheckDebitResponse",
 				"fraudCheckResponse",
-				"giftCardAuthReversalResponse"
+				"giftCardAuthReversalResponse",
+				"giftCardCreditResponse"
 		);
 		
 		if (in_array ( $this->xml_reader->localName, $tracked_elements_names ) && $this->xml_reader->nodeType != \XMLReader::END_ELEMENT) {
