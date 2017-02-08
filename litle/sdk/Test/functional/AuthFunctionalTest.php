b4d6026b1b6b02ceb8c24a91f659fe6f0b3f85b0
qzhao <zappy0109@gmail.com>
2017-02-08 15:34:09 -0500

    Added test_simple_auth_with_networkTransactionId

--- litle/sdk/Test/functional/AuthFunctionalTest.php
+++ litle/sdk/Test/functional/AuthFunctionalTest.php
@@ -205,7 +205,7 @@ class AuthFunctionalTest extends \PHPUnit_Framework_TestCase
     	$this->assertEquals('000',$response);
     }
     
-    public function test_simple_auth_with_card_processingType()
+    public function test_simple_auth_with_card_processingType_originalNetworkTransactionId_originalTransactionAmount()
     {
     	$hash_in = array('id' => 'id',
     			'card'=>array('type'=>'VI',
@@ -228,5 +228,27 @@ class AuthFunctionalTest extends \PHPUnit_Framework_TestCase
     	$this->assertEquals('000',$response);
     }
     
+    public function test_simple_auth_with_networkTransactionId()
+    {
+    	$hash_in = array('id' => 'id',
+    			'card'=>array('type'=>'VI',
+    					'number'=>'4100000000000000',
+    					'expDate'=>'1213',
+    					'cardValidationNum' => '1213'),
+    			'id'=>'1211',
+    			'orderId'=> '2111',
+    			'reportGroup'=>'Planets',
+    			'orderSource'=>'ecommerce',
+    			'amount'=>'0',
+    			'processingType' => 'initialRecurring',
+    			'originalNetworkTransactionId' => 'abcdefghijklmnopqrstuvwxyz',
+    			'originalTransactionAmount' => '1000'
+    	);
+    
+    	$initilaize = new LitleOnlineRequest();
+    	$authorizationResponse = $initilaize->authorizationRequest($hash_in);
+    	$response = XmlParser::getNode($authorizationResponse,'networkTransactionId');
+    	$this->assertEquals('63225578415568556365452427825',$response);
+    }
 
 }
