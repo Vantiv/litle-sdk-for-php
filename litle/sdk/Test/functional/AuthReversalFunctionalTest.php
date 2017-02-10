3fbb6f80edc83874430029136ef2b14e89433caf
csharma <csharma@csharma-vm2.dwi.litle.com>
2017-02-10 10:14:39 -0500

    Chnaged response code from 1 to 0 as in xml11 1 is not expected ever

--- litle/sdk/Test/functional/AuthReversalFunctionalTest.php
+++ litle/sdk/Test/functional/AuthReversalFunctionalTest.php
@@ -36,7 +36,7 @@ class AuthReversalFunctionalTest extends \PHPUnit_Framework_TestCase
         $initialize = new LitleOnlineRequest();
         $authReversalResponse = $initialize->authReversalRequest($hash_in);
         $response = XmlParser::getNode($authReversalResponse,'response');
-        $this->assertEquals('001',$response);
+        $this->assertEquals('000',$response);
     }
 
 function test_simple_authreversal_fields_out_of_order()
@@ -48,7 +48,7 @@ function test_simple_authreversal_fields_out_of_order()
         $initialize = new LitleOnlineRequest();
         $authReversalResponse = $initialize->authReversalRequest($hash_in);
         $response = XmlParser::getNode($authReversalResponse,'response');
-        $this->assertEquals('001',$response);
+        $this->assertEquals('000',$response);
     }
 
 }
