a4d3b5b8584b08b135ad7e5797c576e0000df8e5
csharma <csharma@csharma-vm2.dwi.litle.com>
2017-02-16 13:26:42 -0500

    Removing ID restrictions change

--- litle/sdk/Test/functional/AuthFunctionalTest.php
+++ litle/sdk/Test/functional/AuthFunctionalTest.php
@@ -309,23 +309,5 @@ class AuthFunctionalTest extends \PHPUnit_Framework_TestCase
     	
     }
     
-    public function test_simple_auth_with_card_IdRestrictions()
-    {
-    	$hash_in = array('id' => 'id',
-    			'card'=>array('type'=>'VI',
-    					'number'=>'4100000000000000',
-    					'expDate'=>'1213',
-    					'cardValidationNum' => '1213'),
-    			'id'=>'',
-    			'orderId'=> '2111',
-    			'reportGroup'=>'Planets',
-    			'orderSource'=>'ecommerce',
-    			'amount'=>'0');
-    
-    	$initialize = new LitleOnlineRequest();
-    	$authorizationResponse = $initialize->authorizationRequest($hash_in);
-    	$message= XmlParser::getAttribute($authorizationResponse,'litleOnlineResponse','message');
-    	$this->assertRegExp('/Error validating xml data against the schema/',$message);
-    }
 
 }
