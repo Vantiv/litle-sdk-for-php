641a7f49843fc1403822aba326c123f2065ecb8e
Brian Arnold <sdksupport@litle.com>
2017-08-17 16:44:05 -0400

    update Sandbox URL to be current

--- litle/sdk/UrlMapper.php
+++ litle/sdk/UrlMapper.php
@@ -39,7 +39,7 @@ class UrlMapper
     {
         $litleOnlineCtx = 'vap/communicator/online';
         if ($litleEnv == UrlMapper::SANDBOX)
-            return 'https://www.testlitle.com/sandbox/communicator/online';
+            return 'https://www.testvantivcnp.com/sandbox/communicator/online';
         elseif ($litleEnv == UrlMapper::POSTLIVE)
             return 'https://postlive.litle.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::BETACERT)
@@ -57,6 +57,6 @@ class UrlMapper
         elseif ($litleEnv == UrlMapper::PRELIVE)
             return 'https://prelive.litle.com/' . $litleOnlineCtx;
         else
-            return 'https://www.testlitle.com/sandbox/communicator/online';
+            return 'https://www.testvantivcnp.com/sandbox/communicator/online';
     }
 }
