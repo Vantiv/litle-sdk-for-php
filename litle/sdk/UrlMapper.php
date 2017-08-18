4281c5fa4f60f05e009a8092a118a08da5b83cbe
Brian Arnold <sdksupport@litle.com>
2017-08-18 10:22:59 -0400

    update URLs to be current

--- litle/sdk/UrlMapper.php
+++ litle/sdk/UrlMapper.php
@@ -26,13 +26,11 @@ namespace litle\sdk;
 class UrlMapper
 {
     const POSTLIVE = "postlive";
-    const BETACERT = "betacert";
     const SANDBOX = "sandbox";
     const PRODUCTION = "production";
     const TRANSACT_PRODUCTION = "transact_production";
     const TRANSACT_PRELIVE = "transact_prelive";
     const TRANSACT_POSTLIVE = "transact_postlive";
-    const TRANSACT_BETACERT = "transact_betacert";
     const PRELIVE = "prelive";
 
     public static function getUrl($litleEnv)
@@ -41,21 +39,17 @@ class UrlMapper
         if ($litleEnv == UrlMapper::SANDBOX)
             return 'https://www.testvantivcnp.com/sandbox/communicator/online';
         elseif ($litleEnv == UrlMapper::POSTLIVE)
-            return 'https://postlive.litle.com/' . $litleOnlineCtx;
-        elseif ($litleEnv == UrlMapper::BETACERT)
-            return 'https://betacert.litle.com/' . $litleOnlineCtx;
+            return 'https://payments.vantivpostlive.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::PRODUCTION)
-            return 'https://payments.litle.com/' . $litleOnlineCtx;
+            return 'https://payments.vantivcnp.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::TRANSACT_PRODUCTION)
-            return 'https://transact.litle.com/' . $litleOnlineCtx;
+            return 'https://transact.vantivcnp.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::TRANSACT_PRELIVE)
-            return 'https://transact-prelive.litle.com/' . $litleOnlineCtx;
+            return 'https://transact.vantivprelive.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::TRANSACT_POSTLIVE)
-            return 'https://transact-postlive.litle.com/' . $litleOnlineCtx;
-        elseif ($litleEnv == UrlMapper::TRANSACT_BETACERT)
-            return 'https://transact-betacert.litle.com/' . $litleOnlineCtx;
+            return 'https://transact.vantivpostlive.com/' . $litleOnlineCtx;
         elseif ($litleEnv == UrlMapper::PRELIVE)
-            return 'https://prelive.litle.com/' . $litleOnlineCtx;
+            return 'https://payments.vantivprelive.com/' . $litleOnlineCtx;
         else
             return 'https://www.testvantivcnp.com/sandbox/communicator/online';
     }
