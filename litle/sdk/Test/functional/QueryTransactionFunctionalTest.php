5eed80be1f798f2b93166c72d85a39c499413fbc
chahatSharma <sharma.ch@husky.neu.edu>
2017-12-11 14:40:55 -0500

    for testing purpose || changed childnodes to getelementsbytagname || need to revisit

--- litle/sdk/Test/functional/QueryTransactionFunctionalTest.php
+++ litle/sdk/Test/functional/QueryTransactionFunctionalTest.php
@@ -82,7 +82,7 @@ class QueryTransactionFunctionalTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('000', $response);
         $this->assertEquals('2', $matchCount);
         $resultsMax10 = XmlParser::getNodeWithChildren($queryTransactionResponse, 'results_max10');
-        foreach ($resultsMax10->childNodes as $child) {
+        foreach ($resultsMax10->getElementsByTagName('authorizationResponse') as $child) {
             $childResponse = XmlParser::getNode($child, 'response');
             $childMessage = XmlParser::getNode($child, 'message');
             $childOrderId = XmlParser::getNode($child, 'orderId');
